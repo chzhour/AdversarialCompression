e567bfd825127eebe11934d03ad3e985d2e1f0fc
chzhour <chen@chendeMacBook-Pro.local>
2018-06-09 01:55:15 +0800

    Remove duplicate, add shuffle, add visualization

---
+++ visualize.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import numpy as np
+from matplotlib import pyplot
+
+
+def extract_acy(line):
+	acy = line.strip('\n').split(' ')[-1]
+	acy = float(acy[1:-2])
+	return acy
+
+def visualize(filename, color):
+	start = False
+	accuracies = []
+	count = 0
+	with open(filename) as infile:
+		for line in infile:
+			if "Accuracy of D" in line:
+				start = True
+			elif start and line.startswith("Test set:"):
+				count += 1
+				if count == 1:
+					print(filename + "\t\tTeacher: {:.2f} \t\tStudent: ".format(extract_acy(line)), end=' ')
+				elif count%2 == 0:
+					accuracies.append( extract_acy(line) )
+
+	accuracies = np.array(accuracies)
+	print(accuracies.max())
+	pyplot.plot(accuracies, color)
+
+
+visualize('/Users/chen/Documents/Dropbox/github/AdversarialCompression/parallel_deeperD.txt', 'red')
+visualize('/Users/chen/Desktop/parallel_0.01_deeperD_shuffledata.txt', 'blue')
+visualize('/Users/chen/Desktop/parallel_0.01_deeperD_duplicatedata.txt', 'green')
+visualize('/Users/chen/Desktop/parallel_0.01_deeperD2_duplicatedata.txt', 'black')
+pyplot.show()
