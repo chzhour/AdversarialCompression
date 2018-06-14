da6de9954e877ab672b55f49ccf185848dfc4b7a
chzhour <chzhour@mail2.sysu.edu.cn>
2018-06-14 11:43:25 +0800

    Add multi_students

---
+++ multi_students.py
@@ -0,0 +1,330 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = features = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        #return F.log_softmax(x, dim=1)
+        #feature = F.dropout(features, training=self.training) #dropout????
+        return x, features.view(x.size()[0], -1)
+
+class Discriminator(nn.Module):
+    #first:     input - 200 - 100 - 2
+    #deeperD:   input - 400 - 400 - 100 - 2, good in most cases
+    #deeperD2:  input - 400 - 400 - 400 - 400 - 2
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.operation = nn.Sequential( nn.Linear(10 + 1, 400), #10+1
+                                        nn.ReLU(),
+                                        nn.Dropout(),
+
+                                        nn.Linear(400, 400),
+                                        nn.ReLU(),
+                                        nn.Dropout(),
+
+                                        nn.Linear(400, 100),
+                                        nn.ReLU(),
+                                        nn.Dropout(),
+
+                                        nn.Linear(100, 4)   )
+
+    def forward(self, x):
+        x = self.operation(x)
+        return F.log_softmax(x, dim=1)
+
+def reverse_grad_hook(self, grad_input, grad_output):
+    assert(len(grad_input) == 3)
+    return grad_input[0]*-1, grad_input[1]*-1, grad_input[2]*-1
+
+def train_teacher(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output, _ = model(data)
+        #loss = F.nll_loss(output, target)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output, _ = model(data)
+            #test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
+            test_loss += F.cross_entropy(output, target, size_average=False).item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    acy = 1.0 * correct / len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * acy))
+    return acy
+
+def train_student_batch(args, teacher_model, student_model, discriminator, device, train_loader, optimizer, epoch):
+    teacher_model.train()
+    student_model.train()
+    discriminator.train()
+    for batch_idx, (data, c_target) in enumerate(train_loader):
+        data, c_target = data.to(device), c_target.to(device)
+        optimizer.zero_grad()
+
+        with torch.no_grad():
+            teacher_output, teacher_features = teacher_model(data)
+        student_output, _ = student_model(data)
+
+        classifier_output = torch.cat((teacher_output, student_output), dim=0) #tensor
+        classifier_output = (classifier_output,
+                            torch.cat((teacher_features, teacher_features), dim=0)) #tuple
+        classifier_output = torch.cat(classifier_output, dim=1) #tensor
+        c_target = c_target.float().unsqueeze_(dim=1)*1.0 # int64 to float32, (size) to (size, 1)
+        c_target = torch.cat((c_target, c_target), dim=0) #duplicated tensor
+        classifier_output = torch.cat((classifier_output, c_target), dim=1)
+
+        d_target = (torch.zeros(data.size()[0], dtype=torch.int64), torch.ones(data.size()[0], dtype=torch.int64))
+        d_target = torch.cat(d_target, dim=0).to(device)
+
+        d_output = discriminator(classifier_output)
+        loss = F.nll_loss(d_output, d_target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()), end='\t')
+
+            print('Accuracy of D: {:.2f}%'.format( 
+                    100.0* (d_output.max(dim=1)[1] == d_target).sum().item() / d_target.size()[0] ))
+
+def train_student_parallel(args, teacher_model, student_model, discriminator, device, train_loader, 
+                            optimizer_student, optimizer_discriminator, epoch):
+    teacher_model.train()
+    student_model.train()
+    discriminator.train()
+    for batch_idx, (data, c_target) in enumerate(train_loader):
+        data, c_target = data.to(device), c_target.to(device)
+        rand_permute = torch.randperm(data.size()[0]).to(device)
+        data, c_target = data[rand_permute], c_target[rand_permute]
+        optimizer_student.zero_grad()
+        optimizer_discriminator.zero_grad()
+
+        with torch.no_grad():
+            teacher_output, teacher_features = teacher_model(data) 
+        student_output, _ = student_model(data)
+        assert(data.size()[0]%2==0)
+        half_idx = int(data.size()[0]/2)
+
+        classifier_output = torch.cat((teacher_output[:half_idx], student_output[:half_idx]), dim=1) #tensor
+        classifier_output = (classifier_output, 
+                            torch.cat((student_output[half_idx:], teacher_output[half_idx:]), dim=1) ) #tuple
+        classifier_output = torch.cat(classifier_output, dim=0) #tensor
+        c_target = c_target.float().unsqueeze_(dim=1)*1.0 #int64 to float32, (size) to (size, 1)
+        classifier_output = torch.cat((classifier_output, c_target), dim=1)
+
+        d_target = (torch.zeros(half_idx, dtype=torch.int64), torch.ones(half_idx, dtype=torch.int64))
+        d_target = torch.cat(d_target, dim=0).to(device)
+
+        d_output = discriminator(classifier_output, data)
+        loss = F.nll_loss(d_output, d_target)
+        loss.backward()
+        optimizer_student.step()
+        #if torch.rand(1).item() < 0.5:
+        optimizer_discriminator.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()), end='\t')
+
+            print('Accuracy of D: {:.2f}%'.format( 
+                    100.0* (d_output.max(dim=1)[1] == d_target).sum().item() / d_target.size()[0] ))
+
+def train_multi_students(args, teacher_model, student_models, discriminator, device, train_loader, 
+                            optimizer_student, optimizer_discriminator, epoch):
+    teacher_model.train()
+    for m in student_models:
+        m.train()
+    discriminator.train()
+    for batch_idx, (data, c_target) in enumerate(train_loader):
+        data, c_target = data.to(device), c_target.to(device)
+        optimizer_student.zero_grad()
+        optimizer_discriminator.zero_grad()
+
+        classifier_output = []
+        with torch.no_grad():
+            classifier_output.append( teacher_model(data)[0] )
+        for m in student_models:
+            classifier_output.append( m(data)[0] )
+        classifier_output = torch.cat(classifier_output, dim=0)
+        c_target = c_target.float().unsqueeze_(dim=1)*1.0 # int64 to float32, (size) to (size, 1) for cat
+        c_target = c_target.repeat(len(student_models)+1, 1) #duplicated tensor
+        classifier_output = torch.cat((classifier_output, c_target), dim=1)
+
+        d_target = [torch.zeros(data.size()[0], dtype=torch.int64)]
+        for i in range(len(student_models)):
+            d_target.append( torch.ones(data.size()[0], dtype=torch.int64) * (i+1) )
+        d_target = torch.cat(d_target, dim=0).to(device)
+
+        d_output = discriminator(classifier_output)
+        loss = F.nll_loss(d_output, d_target)
+        loss.backward()
+        optimizer_student.step()
+        optimizer_discriminator.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()), end='\t')
+
+            print('Accuracy of D: {:.2f}%'.format( 
+                    100.0* (d_output.max(dim=1)[1] == d_target).sum().item() / d_target.size()[0] ))
+
+def test_multi_models(args, models, device, test_loader):
+    for model in models:
+        model.eval()
+    test_loss = [0] * len(models)
+    correct = [0] * len(models)
+    correct_merged = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            for idx, model in enumerate(models):
+                output, _ = model(data)
+                prob = F.softmax(output, dim=1)
+                if idx == 0:
+                    cumulative_prob = prob
+                else:
+                    cumulative_prob += prob
+
+                test_loss[idx] += F.nll_loss(torch.log(prob), target, size_average=False).item() # sum up batch loss
+                pred = output.max(1, keepdim=True)[1] # get the index of the max logits before softmax
+                correct[idx] += pred.eq(target.view_as(pred)).sum().item()
+            pred = cumulative_prob.max(1, keepdim=True)[1]
+            correct_merged += pred.eq(target.view_as(pred)).sum().item()
+
+    for idx in range(len(models)):
+        print('Student {}\tTest Average Loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+            idx+1, test_loss[idx]/len(test_loader.dataset), correct[idx], len(test_loader.dataset),
+            100. * correct[idx] / len(test_loader.dataset)
+            ))
+
+    acy = 1.0 * correct_merged / len(test_loader.dataset)
+    print('\nAccuracy of Merged Students: {}/{} ({:.2f}%)\n'.format(
+        correct_merged, len(test_loader.dataset), 100. * acy))
+    return acy
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=1000, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    args = parser.parse_args()
+    print(args)
+
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+
+    teacher_model = Net().to(device)
+    optimizer = optim.SGD(teacher_model.parameters(), lr=0.01, momentum=args.momentum)
+    print(teacher_model)
+    print(optimizer)
+
+    teacher_acy = 0
+    for epoch in range(1, 2):
+        train_teacher(args, teacher_model, device, train_loader, optimizer, epoch)
+        teacher_acy = test(args, teacher_model, device, test_loader)
+
+    ##################################################################
+    student_models = []
+    optimizer_student = [] #list of parameters, will be changed to optimizer
+    for i in range(3):
+        student_models.append( Net().to(device) )
+        student_models[-1].fc2.register_backward_hook(reverse_grad_hook)
+        optimizer_student += list(student_models[-1].parameters())
+    discriminator = Discriminator().to(device)
+    #optimizer = optim.SGD(list(student_model.parameters()) + list(discriminator.parameters()), lr=args.lr, momentum=args.momentum)
+    optimizer_student = optim.SGD(optimizer_student, lr=args.lr, momentum=args.momentum)
+    optimizer_discriminator = optim.SGD(discriminator.parameters(), lr=args.lr, momentum=args.momentum)
+    print(student_models[0])
+    print(discriminator)
+    #print(optimizer)
+    print(optimizer_student)
+    print(optimizer_discriminator)
+
+    best_student_acy = -1.0
+    for epoch in range(1, args.epochs + 1):
+        train_multi_students(args, teacher_model, student_models, discriminator, device, train_loader, 
+                                optimizer_student, optimizer_discriminator, epoch)
+        if epoch == 1:
+            assert(teacher_acy == test(args, teacher_model, device, test_loader))
+        else:
+            print('Teacher Test Accuracy: {:.2f}%\n'.format(100.0 * teacher_acy))
+        acy = test_multi_models(args, student_models, device, test_loader)
+        
+        if(best_student_acy < acy):
+            best_student_acy = acy
+        print('Best Test Accuracy of Merged Students: {:.2f}%\n'.format(100.0 * best_student_acy))
+
+
+
+if __name__ == '__main__':
+    main()
--- visualize.py
+++ visualize.py
@@ -30,9 +30,29 @@ def visualize(filename, color):
 	pyplot.plot(accuracies, color)
 
 
-visualize('/Users/chen/Desktop/parallel_features_deeperD_shuffledata.txt', 'red')
+def visualize_multi(filename, color):
+	accuracies = []
+	has_teacher = False
+	with open(filename) as infile:
+		for line in infile:
+			# if "Teacher Test Accuracy:" in line and not has_teacher:
+			# 	print(filename + " " + color + 
+			# 			"\t\tTeacher: {:.2f} \t\tStudent: ".format())
+			if line.startswith("Accuracy of Merged Students:"):
+				accuracies.append( extract_acy(line) )
+
+	accuracies = np.array(accuracies)
+	print(accuracies.max())
+	pyplot.plot(accuracies, color)
+
+
+#visualize('/Users/chen/Desktop/parallel_features_deeperD_shuffledata.txt', 'red')
 visualize('/Users/chen/Documents/Dropbox alias/AdversarialCompression/parallel_deeperD_shuffledata.txt', 'blue')
-visualize('/Users/chen/Desktop/parallel_featuresconv1_deeperD_shuffledata.txt', 'green')
-visualize('/Users/chen/Desktop/parallel_Dcnnfeatures_deeperD_shuffledata.txt', 'black')
+visualize('/Users/chen/Desktop/parallel_featuresconv1_deeperD_shuffledata.txt', 'green') #could be good
+visualize('/Users/chen/Desktop/parallel_Dcnnfeatures_deeperD_shuffledata.txt', 'black') #could be good
+#visualize_multi('/Users/chen/Desktop/batch_3students_deeperD2.txt', 'pink')
+visualize('/Users/chen/Desktop/parallel_Dcnnfeatures_lr0.05_deeperD_shuffledata.txt', 'red')
+visualize('/Users/chen/Desktop/parallel_Dcnnfeatures_lr0.0025_deeperD_shuffledata.txt', 'pink')
+visualize('/Users/chen/Desktop/parallel_deeperD_shuffledata2.txt', 'orange')
 #visualize('/Users/chen/Desktop/parallel_deeperD2_shuffledata.txt', 'pink')
 pyplot.show()
